980a1b46bc988001f4162cdce0b5009c5781539a
IrfanAfifuddin <161239139+IrfanAfifuddin@users.noreply.github.com>
2025-04-29 00:16:25 +0700

    Add files via upload

---
+++ pertemuan 6/jsCH5.js
@@ -0,0 +1,11 @@
+Video.addEventListener("click", function video(){
+    alert("ini video slash solo");
+});
+
+Audio.addEventListener("click", function Audio(){
+    alert("ini audio slash solo");
+});
+
+Images.addEventListener("click", function Images(){
+    alert("ini gamabar slash solo");
+});
